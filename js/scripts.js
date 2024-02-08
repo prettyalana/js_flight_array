f67cf5898e4a65259a8b9c4fbb1fe74260771d34
Sean R Reid <sean@torchcodelab.com>
2024-02-08 08:46:34 -0500

    Add links to MDN

--- js/scripts.js
+++ js/scripts.js
@@ -14,10 +14,13 @@ document.addEventListener('DOMContentLoaded', async () => {
     console.log(myArray);
 
     // Sort and return the data based on the airline
+    // hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#sorting_array_of_objects
 
     // Sort and return the data based on the arrival airport
 
     // Filter out everything but the flights made by Delta, return the new data
+    // hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter#filtering_invalid_entries_from_json
 
     // Do the same as before, but try doing it with reduce() instead of filter
+    // hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce#sum_of_values_in_an_object_array
 });
